68cb7cbdb753382915ef3ffffb82808e48135b60
Rodolphe CHABI ILOUGBADE <rchabi@it4life.org>
2023-01-07 13:27:30 +0000

    resolve secret key expose

--- chapitre1/mysite/mysite/settings.py
+++ chapitre1/mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k^y!y+ax1@p#v_@1085bqzx(j$=7z3g3i*xw(iqqes!ku=5$^t'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
